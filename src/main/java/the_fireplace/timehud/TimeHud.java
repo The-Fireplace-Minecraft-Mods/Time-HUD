fe8b867b82daab62f0619c97862eef9719f3d78e
The_Fireplace <the.f1repl4ce@gmail.com>
2015-11-08 20:43:48 -0600

    Initial commit

---
+++ src/main/java/the_fireplace/timehud/TimeHud.java
@@ -0,0 +1,16 @@
+package the_fireplace.timehud;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid=TimeHud.MODID, name=TimeHud.MODNAME, version=TimeHud.VERSION)
+public class TimeHud {
+	public static final String MODID="timehud";
+	public static final String MODNAME="Time HUD";
+	public static final String VERSION="2.0.0.1";
+	@EventHandler
+	public void init(FMLInitializationEvent event){
+		
+	}
+}
